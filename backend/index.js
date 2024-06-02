0c169d05ff2f82e818f6a42bbecf2a405de630a6
s4nat <sanatkhandekar@gmail.com>
2024-06-03 06:52:14 +0800

    options

--- backend/index.js
+++ backend/index.js
@@ -17,14 +17,12 @@ connectDB();
 const app = express();
 
 // Handle preflight requests
-app.use((req, res, next) => {
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
-        res.status(200).send();
-    }
-    next();
-})
+app.options('*', (req, res) => {
+    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.sendStatus(200);
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());

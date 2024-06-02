1fd87270067ddb270aa67b0738bed55fec3fec0c
s4nat <sanatkhandekar@gmail.com>
2024-06-03 05:29:38 +0800

    cors workaround

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 //packages
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -16,10 +15,18 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-    origin: "*", // Allow all origins
-    credentials: true, // Allow credentials
-}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());

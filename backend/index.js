f391cc8efe847b0154afd163a9fa1de8ca2ec4ad
s4nat <sanatkhandekar@gmail.com>
2024-06-03 06:38:00 +0800

    getting desp now

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,14 @@ connectDB();
 const app = express();
 
 // Handle preflight requests
-app.options('*', (req, res) => {
-    res.sendStatus(200); // Respond with OK status for OPTIONS requests
-});
+app.use((req, res, next) => {
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.status(200).send();
+    }
+})
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
